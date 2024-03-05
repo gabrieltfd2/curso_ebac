44ba31815a40ffec633e7e195c914f5a344727bd
Gabriel Trindade <141684234+gabrieltfd2@users.noreply.github.com>
2024-03-05 17:50:20 -0300

    first commit

---
+++ src/teste.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
---
+++ src/teste.tsx
@@ -0,0 +1,7 @@
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
